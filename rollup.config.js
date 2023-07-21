3aed1d85a5dfb807989907b7af987280565e60ba
Rizky Saskia Putra <36327242+rizkysaskiaputra@users.noreply.github.com>
2023-07-21 22:02:17 +0700

    added: typescript plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,19 @@
-export default [
-  {
-    input: "src/lib/main.js",
-    watch: true,
-    output: { file: "src/bundle.js", format: "umd" },
-  },
-];
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+
+export default {
+  input: "src/lib/main.ts",
+  watch: true,
+  output: { file: "src/bundle.min.js", format: "umd" },
+  plugins: [
+    typescript(),
+    terser({
+      mangle: {
+        toplevel: true,
+      },
+      compress: {
+        ecma: 2019,
+      },
+    }),
+  ],
+};
